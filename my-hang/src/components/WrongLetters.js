64ff6679b107c1d2feaa6d9d917ce20e7d5b5ece
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-06-09 19:19:47 +0300

    wrongletters-file

--- my-hang/src/components/WrongLetters.js
+++ my-hang/src/components/WrongLetters.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 const WrongLetters = ({ wrongLetters }) => {
 
+  // if it is greater than zero and add a paragraph saying wrong letters
+  // we mapping the letter and the index we adding the span in every single time
+  // each letter will be inside this span, where we might need also a comma
+  // in our reduce we have the previous and current values
+  // so if previous values are equal null then we have our current else we have our previous
+  // adding a coma in between and then we have our current value
   return (
     <div className="wrong-letters-container">
       <div>
